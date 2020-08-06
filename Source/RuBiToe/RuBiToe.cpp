c67c55b11266e86a556f6cfa0b78b268a6947d09
William Clemmons <wacboss@live.com>
2020-08-06 13:16:50 -0500

    Initial commit

---
+++ Source/RuBiToe/RuBiToe.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RuBiToe.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RuBiToe, "RuBiToe" );
