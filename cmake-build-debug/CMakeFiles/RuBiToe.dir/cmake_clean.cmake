197853913cb2fef6196067f7daf8f94bd145d85f
William Clemmons <wacboss@live.com>
2020-12-01 02:14:41 -0600

    Moving with C++
    The cube now moves based on a line of code that calls the blueprint function

---
+++ cmake-build-debug/CMakeFiles/RuBiToe.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RuBiToe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/RuBiToe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
